ec2bcc3966a47f8a4ee3af3ecc8d667ce7b0c305
LucaPalminteri <lucapalminteri02@gmail.com>
2022-12-11 15:45:39 -0300

    change asm file

--- fib.asm
+++ fib.asm
@@ -1,27 +1,27 @@
-0000000100000f20    pushq   %rbp
-0000000100000f21    movq    %rsp, %rbp
-0000000100000f24    subq    $0x20,% rsp
-0000000100000f28    movl    $0x0, -0x4(%rbp)
+0x100000f20    pushq   %rbp
+0x100000f21    movq    %rsp, %rbp
+0x100000f24    subq    $0x20,% rsp
+0x100000f28    movl    $0x0, -0x4(%rbp)
 
-0000000100000f2f    movl    $0x0, -0x8(%rbp)
-0000000100000f36    movl    $0x1, -0c(%rbp)
+0x100000f2f    movl    $0x0, -0x8(%rbp)
+0x100000f36    movl    $0x1, -0c(%rbp)
 
-0000000100000f3d    leaq    0x56(%rip), %rdi
-0000000100000f44    movl    -0x8(%rbp) %esi
-0000000100000f47    movb    $0x0, %al
-0000000100000f49    callq   0x100000f78
+0x100000f3d    leaq    0x56(%rip), %rdi
+0x100000f44    movl    -0x8(%rbp) %esi
+0x100000f47    movb    $0x0, %al
+0x100000f49    callq   0x100000f78
 
-0000000100000f4e    movl    -0x8(%rbp), %esi
-0000000100000f51    addl    -0xc(%rbp), %esi
-0000000100000f54    movl    %esi, -0x10(%rbp)
+0x100000f4e    movl    -0x8(%rbp), %esi
+0x100000f51    addl    -0xc(%rbp), %esi
+0x100000f54    movl    %esi, -0x10(%rbp)
 
-0000000100000f57    movl    -0xc(%rbp), %esi
-0000000100000f5a    movl    %esi, -0x8(%rbp)
+0x100000f57    movl    -0xc(%rbp), %esi
+0x100000f5a    movl    %esi, -0x8(%rbp)
     
-0000000100000f5d    movl    -0x10(%rbp), %esi
-0000000100000f60    movl    %esi, -0xc(%rbp)
+0x100000f5d    movl    -0x10(%rbp), %esi
+0x100000f60    movl    %esi, -0xc(%rbp)
 
-0000000100000f63    movl    %eax, -0x14(%rbp)
-0000000100000f66    cmpl    $0xff, -0x8(%rbp)
-0000000100000f6d    jl      0x100000f3d
-0000000100000f73    jmp     0x100000f2f
+0x100000f63    movl    %eax, -0x14(%rbp)
+0x100000f66    cmpl    $0xff, -0x8(%rbp)
+0x100000f6d    jl      0x100000f3d
+0x100000f73    jmp     0x100000f2f
